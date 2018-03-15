c79db20f1caa5030d21b37f1af2bd8328c11f3e6
rlerch <ryan.lerch@sungard.com>
2018-03-15 08:10:59 -0400

    Initial Commit

---
+++ ValidParens.Tests/ValidatorTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ValidParens.Tests
+{
+    [TestClass]
+    public class ValidatorTests
+    {
+        private ParenValidator _validator;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _validator = new ParenValidator();
+        }
+
+        [TestMethod]
+        public void EmptyString_IsValid()
+        {
+            var toValidate = string.Empty;
+
+            var toCheck = _validator.IsValid(toValidate);
+
+            Assert.IsTrue(toCheck);
+        }
+
+        [TestMethod]
+        public void MatchingParens_AreValid()
+        {
+            var toValidate = "()";
+
+            var toCheck = _validator.IsValid(toValidate);
+
+            Assert.IsTrue(toCheck);
+        }
+
+        [TestMethod]
+        public void NonMatchingParents_AreNotValid()
+        {
+            var toValidate = "(";
+
+            var toCheck = _validator.IsValid(toValidate);
+
+            Assert.IsFalse(toCheck);
+        }
+
+        [TestMethod]
+        public void NestedParens_AreValid()
+        {
+            var toValidate = "(())";
+
+            var toCheck = _validator.IsValid(toValidate);
+
+            Assert.IsTrue(toCheck);
+        }
+
+        [TestMethod]
+        public void NestedParensWithBraces_AreValid()
+        {
+            var toValidate = "({})";
+
+            var toCheck = _validator.IsValid(toValidate);
+
+            Assert.IsTrue(toCheck);
+        }
+
+        [TestMethod]
+        public void NestedParensWithUnmatchedBraces_AreValid()
+        {
+            var toValidate = "({)";
+
+            var toCheck = _validator.IsValid(toValidate);
+
+            Assert.IsFalse(toCheck);
+        }
+
+        [TestMethod]
+        public void GroupsThatEndAfterTheirParents_AreInValid()
+        {
+            var toValidate = "({)}";
+
+            var toCheck = _validator.IsValid(toValidate);
+
+            Assert.IsFalse(toCheck);
+        }
+    }
+}
---
+++ ValidParens/ParenValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ValidParens
+{
+    public class ParenValidator
+    {
+        public bool IsValid(string toValidate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
